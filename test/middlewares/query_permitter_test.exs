38e4f172e91d6ed362c0868dffd5349f70618174
Rafael Scheffer <rafaelscheff@gmail.com>
2019-06-28 16:46:18 -0300

    Remove configuration modules from application env in query permitter test

--- test/middlewares/query_permitter_test.exs
+++ test/middlewares/query_permitter_test.exs
@@ -1,61 +1,68 @@
 defmodule Rajska.QueryPermitterTest do
   use Absinthe.Case, async: true
 
-  defmodule Authorization do
-    use Rajska,
-      otp_app: :my_app,
-      roles: [:user, :admin],
-      all_role: :all
-  end
-
-  Application.put_env(Rajska, :configurator, Authorization)
-
-  defmodule User do
-    defstruct name: "User", email: "email@user.com"
-
-    def __schema__(:source), do: "users"
-  end
-
-  defmodule Schema do
-    use Absinthe.Schema
-
-    def middleware(middleware, field, %Absinthe.Type.Object{identifier: identifier})
-    when identifier in [:query, :mutation, :subscription] do
-      Rajska.add_authentication_middleware(middleware, field)
+  setup_all do
+    defmodule Authorization do
+      use Rajska,
+        otp_app: :my_app,
+        roles: [:user, :admin],
+        all_role: :all
     end
 
-    def middleware(middleware, _field, _object), do: middleware
+    Application.put_env(Rajska, :configurator, Authorization)
 
-    query do
-      field :all_query, :user do
-        middleware Rajska.QueryPermitter, permit: :all
-        resolve fn _, _ ->
-          {:ok, %{name: "bob"}}
+    defmodule User do
+      defstruct name: "User", email: "email@user.com"
+
+      def __schema__(:source), do: "users"
+    end
+
+    defmodule Schema do
+      use Absinthe.Schema
+
+      def middleware(middleware, field, %Absinthe.Type.Object{identifier: identifier})
+      when identifier in [:query, :mutation, :subscription] do
+        Rajska.add_authentication_middleware(middleware, field)
+      end
+
+      def middleware(middleware, _field, _object), do: middleware
+
+      query do
+        field :all_query, :user do
+          middleware Rajska.QueryPermitter, permit: :all
+          resolve fn _, _ ->
+            {:ok, %{name: "bob"}}
+          end
+        end
+
+        field :user_query, :user do
+          middleware Rajska.QueryPermitter, [permit: :user, scoped: false]
+          resolve fn _, _ ->
+            {:ok, %{name: "bob"}}
+          end
+        end
+
+        field :admin_query, :user do
+          middleware Rajska.QueryPermitter, permit: :admin
+          resolve fn _, _ ->
+            {:ok, %{name: "bob"}}
+          end
         end
       end
 
-      field :user_query, :user do
-        middleware Rajska.QueryPermitter, [permit: :user, scoped: false]
-        resolve fn _, _ ->
-          {:ok, %{name: "bob"}}
-        end
-      end
-
-      field :admin_query, :user do
-        middleware Rajska.QueryPermitter, permit: :admin
-        resolve fn _, _ ->
-          {:ok, %{name: "bob"}}
-        end
+      object :user do
+        field :email, :string
+        field :name, :string
       end
     end
 
-    object :user do
-      field :email, :string
-      field :name, :string
+    Application.put_env(Rajska, :schema, Schema)
+
+    on_exit fn ->
+      Application.delete_env(Rajska, :configurator)
+      Application.delete_env(Rajska, :schema)
     end
   end
-
-  Application.put_env(Rajska, :schema, Schema)
 
   test "Admin query fails for unauthenticated user" do
     assert {:ok, %{errors: errors}} = Absinthe.run(admin_query(), __MODULE__.Schema, context: %{current_user: nil})
