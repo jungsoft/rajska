23b207b3b2d5db5aa46a16488dcfed91fd505d0c
Rafael Scheffer <rafaelscheff@gmail.com>
2019-06-30 16:31:28 -0300

    Rename get_scope_by_field/2 to get_scope_by_field!/2

--- lib/middlewares/field_authorization.ex
+++ lib/middlewares/field_authorization.ex
@@ -30,7 +30,7 @@ defmodule Rajska.FieldAuthorization do
 
   def call(resolution, [object: %{fields: fields} = object, field: field]) do
     is_field_private? = fields[field] |> Type.meta(:private) |> is_field_private?(resolution.source)
-    scope_by = get_scope_by_field(object, is_field_private?)
+    scope_by = get_scope_by_field!(object, is_field_private?)
 
     resolution
     |> authorized?(is_field_private?, scope_by, resolution.source)
@@ -41,9 +41,9 @@ defmodule Rajska.FieldAuthorization do
   defp is_field_private?(private, source) when is_function(private), do: private.(source)
   defp is_field_private?(_private, _source), do: false
 
-  defp get_scope_by_field(_object, false), do: :ok
+  defp get_scope_by_field!(_object, false), do: :ok
 
-  defp get_scope_by_field(object, _private) do
+  defp get_scope_by_field!(object, _private) do
     case Type.meta(object, :scope_by) do
       nil -> raise "No scope_by meta defined for object returned from query #{object.identifier}"
       scope_by_field when is_atom(scope_by_field) -> scope_by_field
