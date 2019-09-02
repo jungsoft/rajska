8294aea7ca9ba4658db1a4b8f57f6d0777cd31d6
Rafael Scheffer <rafaelscheff@gmail.com>
2019-09-02 12:27:41 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rajska.MixProject do
   def project do
     [
       app: :rajska,
-      version: "0.2.2",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
