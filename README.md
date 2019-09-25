2028ae6599d547f48fce6116f42f0959d4627be4
Rafael Scheffer <rafaelscheff@gmail.com>
2019-09-25 16:41:48 -0300

    Bump version to 0.4.0

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ The package can be installed by adding `rajska` to your list of dependencies in 
 ```elixir
 def deps do
   [
-    {:rajska, "~> 0.3.1"},
+    {:rajska, "~> 0.4.0"},
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rajska.MixProject do
   def project do
     [
       app: :rajska,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
